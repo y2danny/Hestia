ffd8faa5c5074023ca1865cc369df1a07f8ebd46
Sagetony <uchetony224@gmail.com>
2022-11-24 14:01:29 +0100

    NFT Contract

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,22 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract RealEstate {
+contract RealEstate is ERC721URIStorage{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Hestia Real Estate", "HRE") {}
+
+    function mint(string memory tokenURI) public returns (uint256){
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
 
 }
