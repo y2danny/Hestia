ad9e78552e0fd0e9054aa92db4b3ace440266d59
Alemoh Rapheal <raphealenike@gmail.com>
2022-11-28 14:23:09 -0800

    Add landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@
 
 // Components
 import Navigation from './components/Navigation';
+import Jumbroton from './components/Jumbroton';
+import Properties from './components/Properties';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 // import Search from './components/Search';
 // import Home from './components/Home';
 
@@ -18,6 +24,11 @@ function App() {
   return (
     <div className="relative bg-white w-[1280px] h-[4142px] overflow-clip">
       <Navigation />
+      <Jumbroton />
+      <Properties />
+      <Blog />
+      <Contact />
+      <Footer />
     </div>
   );
 }
