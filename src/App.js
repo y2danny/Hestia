4dbcc1ea8bff4aa691e87f8e6a1d91169cb13abb
sagetony <uchetony224@gmail.com>
2022-11-29 23:13:13 +0100

    added the connect button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import { useEffect, useState } from 'react';
 // import { ethers } from 'ethers';
 
+import { MoralisProvider } from "react-moralis";
+
 // Components
-import Navigation from './components/Navigation';
-import Jumbroton from './components/Jumbroton';
-import Properties from './components/Properties';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Navigation from "./components/Navigation";
+import Jumbroton from "./components/Jumbroton";
+import Properties from "./components/Properties";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 // import Search from './components/Search';
 // import Home from './components/Home';
@@ -20,16 +22,17 @@ import Footer from './components/Footer';
 // import config from './config.json';
 
 function App() {
-
   return (
-    <div className="relative bg-white w-[1280px] h-[4142px] overflow-clip">
-      <Navigation />
-      <Jumbroton />
-      <Properties />
-      <Blog />
-      <Contact />
-      <Footer />
-    </div>
+    <MoralisProvider initializeOnMount={false}>
+      <div className="relative bg-white w-[1280px] h-[4142px] overflow-clip">
+        <Navigation />
+        <Jumbroton />
+        <Properties />
+        <Blog />
+        <Contact />
+        <Footer />
+      </div>
+    </MoralisProvider>
   );
 }
 
