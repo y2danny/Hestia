8e6b46dc58624bb806ddf6c48333aad810a58a1b
Yekasumah <asumahyeka@gmail.com>
2022-12-04 13:28:32 +0100

    fix: responsiveness

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -12,7 +12,7 @@ const Mint = () => {
 
     return (
     <div className="relative w-full h-full max-w-md md:h-auto">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div className="relative bg-transparent rounded-lg shadow dark:bg-green-900">
             <button onClick={hideDialog} type="button"
                 className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
                 data-modal-toggle="authentication-modal">
@@ -25,13 +25,13 @@ const Mint = () => {
                 <span className="sr-only">Close modal</span>
             </button>
             <div className="px-6 py-6 lg:px-8">
-                <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Property Information</h3>
+                <h3 className="mb-4 text-xl font-medium text-green-900 dark:text-white">Property Information</h3>
                 <form className="space-y-6" action="#">
                     <div>
                         <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                             email</label>
                         <input type="email" name="email" id="email"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                             placeholder="name@company.com" required />
                     </div>
                     <div>
@@ -39,25 +39,25 @@ const Mint = () => {
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                             password</label>
                         <input type="password" name="password" id="password" placeholder="••••••••"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                             required />
                     </div>
                     <div className="flex justify-between">
                         <div className="flex items-start">
                             <div className="flex items-center h-5">
                                 <input id="remember" type="checkbox" value=""
-                                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-white dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
                                     required />
                             </div>
                             <label for="remember"
                                 className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
                                 me</label>
                         </div>
-                        <a href="/" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost
+                        <a href="/" className="text-sm text-green-700 hover:underline dark:text-green-500">Lost
                             Password?</a>
                     </div>
                     <button type="submit"
-                        className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buy</button>
+                        className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buy</button>
                 </form>
               <div className="p-6 text-center">
                 <button data-modal-toggle="popup-modal" type="button"
